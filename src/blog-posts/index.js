c4e78166c8c2698f9a7aa1aa8e33e308d9020a6d
Alex Craig <acraig780@gmail.com>
2021-05-01 19:08:11 -0400

    add new blog posts

--- src/blog-posts/index.js
+++ src/blog-posts/index.js
@@ -1,11 +1,15 @@
 import whyBlog from './why-a-blog.md';
 import mtgaMarch2021 from './mtga-march-2021-retrospect.md';
+import mtgaApril2021 from './mtga-april-2021-retrospect.md';
 import historic from './strixhaven-historic-shakeup.md';
 import archive20210414 from './mtg-deck-archive-2021-04-14.md';
+import frustrations from './frustrations-of-mtga-client.md';
 
 export const posts = [
   whyBlog,
   mtgaMarch2021,
   historic,
   archive20210414,
+  frustrations,
+  mtgaApril2021,
 ].sort((a, b) => a.date > b.date ? -1 : 1);
