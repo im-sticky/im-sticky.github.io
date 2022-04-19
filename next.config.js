c65b0bb5e29c0e5c65540915cfc318d176102334
Alex Craig <acraig780@gmail.com>
2022-04-19 15:32:36 -0400

    command for deploying

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const prod = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: prod ? '' : '',
 };
 
 module.exports = nextConfig;
