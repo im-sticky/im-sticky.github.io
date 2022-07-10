0c656df1f8cc283329c80f8f4db38effa4cc1760
Alex Craig <acraig780@gmail.com>
2022-07-10 11:35:09 -0400

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'normalize.css/normalize.css';
 import 'styles/globals.scss';
+import {GoogleAnalytics, usePageViews} from 'nextjs-google-analytics';
 import {Layout} from 'components/Layout';
 
 import dynamic from 'next/dynamic';
@@ -7,8 +8,11 @@ import dynamic from 'next/dynamic';
 const CardSeerImport = dynamic(() => import('../components/CardSeer'), {ssr: false});
 
 function PortfolioApp({Component, pageProps}) {
+  usePageViews();
+
   return (
     <Layout includeFooter={pageProps.includeFooter}>
+      <GoogleAnalytics />
       <Component {...pageProps} />
       <CardSeerImport />
     </Layout>
