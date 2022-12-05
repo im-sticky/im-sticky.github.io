6589bf019a721b1b33d677666bd3b4db021ffeb6
Alex Craig <acraig780@gmail.com>
2022-12-04 23:11:54 -0500

    full path for meta images

--- components/PageHead/index.js
+++ components/PageHead/index.js
@@ -12,14 +12,14 @@ export function PageHead({title, description, url, image = '/mstile-144x144.png'
       <meta property="og:url" content={`${SITE_URL}/${url}`} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={`${SITE_URL}${image}`} />
 
       <meta name="twitter:site" content="@im_sticky" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:url" content={`${SITE_URL}/${url}`} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={`${SITE_URL}${image}`} />
 
       {children}
     </Head>
