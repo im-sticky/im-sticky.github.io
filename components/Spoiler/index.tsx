e83070b3ec3c0aa5d81b2ad58cf51d4092816447
Alex Craig <acraig@konradgroup.com>
2024-12-28 00:35:48 -0500

    added confetti option to spoiler

--- components/Spoiler/index.tsx
+++ components/Spoiler/index.tsx
@@ -4,14 +4,16 @@ import React, {useState} from 'react';
 import clsx from 'clsx';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+import {confetti} from '@tsparticles/confetti';
 import styles from './index.module.scss';
 
 interface SpoilerProps {
   children: React.ReactNode;
   inline?: boolean;
+  doConfetti?: boolean;
 }
 
-export function Spoiler({inline = false, children}: SpoilerProps) {
+export function Spoiler({inline = false, doConfetti = false, children}: SpoilerProps) {
   const [spoiled, setSpoiled] = useState(false);
   const SpoilerTag = (inline ? 'span' : 'div') as keyof JSX.IntrinsicElements;
 
@@ -23,7 +25,24 @@ export function Spoiler({inline = false, children}: SpoilerProps) {
       })}
     >
       {children}
-      <button type="button" className={styles.spoiler__button} onClick={() => setSpoiled(true)}>
+      <button
+        type="button"
+        className={styles.spoiler__button}
+        onClick={async (e: React.MouseEvent) => {
+          setSpoiled(true);
+
+          if (doConfetti) {
+            const rect = (e.target as HTMLButtonElement).getBoundingClientRect();
+
+            await confetti({
+              position: {
+                x: ((rect.left + rect.width / 2) / window.innerWidth) * 100,
+                y: ((rect.top + rect.height / 2) / window.innerHeight) * 100,
+              },
+            });
+          }
+        }}
+      >
         <span className={styles.spoiler__text}>Reveal&nbsp;Spoiler</span>
         <FontAwesomeIcon icon={faEyeSlash} />
       </button>
